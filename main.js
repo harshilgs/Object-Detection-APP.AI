da9386582a142ef2949e2d3b6d727e2a2e3889f6
harshilgs <74186753+harshilgs@users.noreply.github.com>
2020-11-29 11:39:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 img="";
+status="";
+objects=[];
 
 function setup()
 {
     canvas = createCanvas( 640 , 420);
     canvas.center();
+    objectDetector = ml5.objectDetector( 'cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
 }
 
 function preload()
@@ -14,9 +18,37 @@ function preload()
 function draw()
 {
     image(img , 0 , 0 , 640 , 420);
-    fill("#ff0000");
-    text("Dog And Cat" , 45 , 75);
-    noFill();
-    stroke("#ff0000");
-    rect( 30 , 60 , 540 , 350)
+    
+    if(status !="")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill('#ff0000');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x+15 , objects[i].y+20);
+            noFill();
+            stroke('#ff0000');
+            rect(objects[i].x-15 , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
+
+function modelLoaded()
+{
+    console.log('Model Is Loaded');
+    status = true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error , results)
+{
+      if(error)
+{
+      console.log(error);
+}
+      console.log(results);
+      objects=results;
+}
+
